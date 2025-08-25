99885c4213df2e7a9fed4d19f69ce7922a6ed794
Claudio Amorim <amorim.dotnetdevops@gmail.com>
2025-08-26 04:50:37 +0800

    feat: corrigir tipo de id no schema user.go

--- schemas/user.go
+++ schemas/user.go
@@ -1,11 +1,25 @@
 package schemas
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
-	Id       string `json:"id"`
+	Id       int    `gorm:"primaryKey" json:"id"`
 	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type UserResponse struct {
+	Id        uint      `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at,omitempty"`
 }
